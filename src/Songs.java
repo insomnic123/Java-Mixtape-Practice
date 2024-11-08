21b1dd4fc1257a003d657b6889efe872970e1c4c
insomnic123 <qaziayn@gmail.com>
2024-11-07 20:09:52 -0500

    added even more bugs somehow and comments

--- src/Songs.java
+++ src/Songs.java
@@ -1,5 +1,8 @@
+import java.util.Objects;
+
 public class Songs {
 
+    // Declaring variables
     private String name = "";
     private String song = "";
     private String artist = "";
@@ -7,8 +10,12 @@ public class Songs {
     private int year = 0;
     private int duration = 0;
     private String link = "";
+    public static final String RESET = "\u001B[0m";
+    public static final String BRIGHT_RED = "\u001B[31;1m";
+    public static final String BRIGHT_BLUE = "\u001B[34;1m";
 
-    public Songs(String name, String song, String artist, int year, String genre, int duration, String link){
+    // Constructor Method
+    public Songs(String name, String song, String artist, int year, String genre, int duration, String link) {
         this.name = name;
         this.song = song;
         this.artist = artist;
@@ -18,23 +25,59 @@ public class Songs {
         this.genre = genre;
     }
 
-    public String getName() {return name;}
+    // Getters
+    public String getName() {
+        return name;
+    }
 
-    public String getSong() {return song;}
+    public String getSong() {
+        return song;
+    }
 
-    public String getArtist() {return artist;}
+    public String getArtist() {
+        return artist;
+    }
 
-    public int getYear() {return year;}
+    public int getYear() {
+        return year;
+    }
 
-    public int getDuration() {return duration;}
+    public int getDuration() {
+        return duration;
+    }
 
-    public String link() {return link;}
+    public String link() {
+        return link;
+    }
 
-    public String getGenre() {return genre;}
+    public String getGenre() {
+        return genre;
+    }
 
+    // Method to compare two songs
+    public void compareSongs(Songs a) {
+        if (Objects.equals(a.getGenre(), this.getGenre())) {
+            System.out.println("Both songs are " + a.getGenre());
+        } else {
+            System.out.println(a.getSong() + " by " + a.getArtist() + " is " + a.getGenre() + ", whereas " + this.getSong() + " by " + this.getArtist() + " is " + this.getGenre());
+        }
+        if ((a.getDuration() / 60) == (this.getDuration() / 60)) {
+            System.out.println("Both songs are " + a.getDuration() / 60 + " minutes long.");
+        } else {
+            System.out.println("Both songs are different in length.");
+        }
+        if (a.getYear() == this.getYear()) {
+            System.out.println("Both songs were released in the year " + a.getYear());
+        } else {
+            System.out.println("The two songs were released " + Math.abs(a.getYear() - this.getYear()) + " years apart.");
+        }
+    }
+
+    // toString method
     @Override
     public String toString() {
-//        return ("Submitted by: " + name + ", the song is: " + song + " by " + artist + " released in the year " + year + ". It's a " + genre + " song, coming in at " + duration + " seconds long. Take a listen at: " + link );
-        return (genre + " song: " + song + " | Artist: " + artist + " (" + year + ") " + "| Submitted by: " + name + " | " + link);
+        return String.format(BRIGHT_RED + "%s by %s %n" + BRIGHT_BLUE + "Released: " + RESET + "%d %n" + BRIGHT_BLUE + "Genre:" + RESET + " %s %n" + BRIGHT_BLUE +
+                        "Submitted By:" + RESET + " %s %n" + BRIGHT_BLUE + "Listen at: %s %n" + RESET + "----------",
+                song, artist, year, genre, name, link);
     }
 }
